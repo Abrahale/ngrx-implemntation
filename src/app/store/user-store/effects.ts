e08b62fbaeaa79b872f9ed3c22dbf3e1669bb527
Abrahale <akiros@retrorabbit.co.za>
2021-03-20 07:30:43 +0200

    adding effects

---
+++ src/app/store/user-store/effects.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import * as featureActions from './actions';
+import { createEffect, ofType, Actions } from '@ngrx/effects';
+import { catchError, exhaustMap, map, switchMap } from 'rxjs/operators';
+import {of} from 'rxjs';
+
+@Injectable()
+export class UserEffects{
+    constructor(private action$: Actions){}
+    loadData$ = createEffect(() => this.action$.pipe(
+        ofType<featureActions.LoadRequestAction>(
+            featureActions.ActionTypes.LOAD_REQUEST
+        ),
+        switchMap(() =>
+            this.getUser()
+            .pipe(
+                map(
+                   (data: any) =>
+                    new featureActions.LoadSuccessAction({data})
+                ),
+                catchError((error: string) =>
+                   of(new featureActions.LoadFailureAction({error}))
+                )
+            )
+        )
+    ));
+
+    getUser(): any{
+        return {
+            name: 'Abrahale',
+            surname: 'Kiros',
+            email: 'abrahalekiros@gmail.com',
+            aka: 'Ab_Kiros'
+        };
+    }
+}
+
