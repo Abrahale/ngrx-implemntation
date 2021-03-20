2de8baadaef242dd65450c0e3482e61dcd72dd0d
Abrahale <akiros@retrorabbit.co.za>
2021-03-20 06:37:55 +0200

    adding the user reducer

---
+++ src/app/store/user-store/reducer.ts
@@ -0,0 +1,28 @@
+import { Actions, ActionTypes } from './actions';
+import { State, initalState } from './state';
+
+export function featureReducer(state = initalState, action: Actions): State{
+    switch(action.type){
+        case ActionTypes.LOAD_REQUEST:
+            return{
+                ...state,
+                loading: true,
+                error: ''
+            };
+        
+        case ActionTypes.LOAD_SUCCESS:
+            return {
+                ...state,
+                data: action.payload.data,
+                loading: false,
+                error: ''
+            };
+        
+        case ActionTypes.LOAD_FAILURE:
+            return{
+                ...state,
+                loading:false,
+                error: action.payload.error
+            };
+    }
+}
